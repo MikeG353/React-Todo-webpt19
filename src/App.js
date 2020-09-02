a595d55ace15e9ba46754fae256410e2b14fde94
Michael Gregory <michael.gregory8584@gmail.com>
2020-09-02 12:14:08 -0500

    First commit Initial state added constructor complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
+const todoList = [
+  {
+    name: "Create State",
+    id: Date.now(),
+    completed: false
+  }
+]
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  constructor() {
+    super();
+    this.state = {
+      
+    }
+  }
+
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
